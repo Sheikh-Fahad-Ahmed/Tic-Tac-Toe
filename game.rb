4a581109881b390fbff8aabc8389fbc818a9023d
Fahad Ahmed <fahad838@gmail.com>
2024-09-02 19:07:27 +0530

    Create a game menu for when players start game
    Created a game menu when players start their game.
    Created a loop to switch between player 1 and
    player 2 .

--- game.rb
+++ game.rb
@@ -4,36 +4,61 @@ require_relative 'board'
 require_relative 'player'
 
 class Game
+  attr_accessor :players, :board, :start_game
+
   def initialize(players, board_instance)
     @players = players
     @board = board_instance
-    @@start_game = false
+    @start_game = false
   end
 
-  def display_menu
+  def start_menu
     puts "\n-----Menu-----"
     puts '1. Show board'
-    puts @@start_game ? '2. pick your position' : '2. Start game'
+    puts '2. Start game'
     puts '3. Exit'
     puts '--------------'
     puts "\nEnter your choice:"
     choice = gets.chomp.to_i
-    go_to_menu(choice)
+    start_menu_choice(choice)
   end
 
-  def go_to_menu(choice)
+  def start_menu_choice(choice)
     case choice
     when 1
       @board.show_board
     when 2
-      if @@start_game == false
-        @@start_game = true
-      else
-        'pick your position'
-      end
+      @start_game = true
     when 3
       exit 0
     end
+  end
+
+  def game_menu
+    puts "\n-----Game Menu-----"
+    puts '1. Play turn'
+    puts '2. Show board'
+    puts '3. Exit'
+    puts '-----------'
+    puts "\nEnter your choice:"
+    choice = gets.chomp.to_i
+    game_menu_choice(choice)
+  end
+
+  def game_menu_choice(choice)
+    case choice
+    when 1
+      postion = play_turn(player_num)
+    when 2
+      @board.show_board
+    when 3
+      exit 0
+    end
+  end
+
+  def play_turn
+    puts "\n Enter your postion:"
+    postion = gets.chomp.to_i
   end
 end
 
@@ -43,10 +68,13 @@ players = Player.new
 game1 = Game.new(players, board_instance)
 players.display_player_selection
 board_instance.show_board
+game1.start_menu until game1.start_game == true
+player_num = 0
 loop do
   if board_instance.winning_condition(board_instance.board)
     Exit
   else
-    game1.display_menu
+    puts "Player #{(player_num % 2) + 1}'s Turn"
+    game1.game_menu
   end
 end
