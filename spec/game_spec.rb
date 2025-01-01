acf511ac07da9b2ca217db6a73788fb9af2a4144
Fahad Ahmed <fahad838@gmail.com>
2025-01-01 11:29:53 +0530

    Write test for current_player
    Wrote the test for current_player if it is
    actually getting the value 'X' from player.rb
    or not.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,17 @@
-require './game.rb'
+require './game'
+require './player'
 
 describe Game do
-  
+  subject(:game_one) { described_class.new }
+
+  describe '#initialize' do
+    before do
+      allow_any_instance_of(Player).to receive(:gets).and_return('X')
+    end
+
+    it 'initializes current_player to X' do
+      result = game_one.current_player
+      expect(result).to eq('X')
+    end
+  end
 end
